165f24d51a4460f605da68e7f5ebe5419f408703
Jakob Ruckel <jakob@ruckel.de>
2022-03-26 17:44:02 +0100

    Create GameManager to handle player movement

---
+++ ExplodingChicks/Assets/GameManager/GameManager.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GameManager : MonoBehaviour {
+    public static GameManager Instance = null;
+
+    [SerializeField]
+    private Tilemap map;
+
+    private List<PlayerController> playerControllers = new List<PlayerController>();
+    private PlayerController activePlayer = null;
+
+    
+    private List<Vector3Int> highlightedTiles = new List<Vector3Int>();
+    private void MouseLook() {
+        var pointerWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3Int pointer = map.WorldToCell(pointerWorld);
+        pointer.z = 0;
+
+        if (highlightedTiles.Contains(pointer)) {
+            return;
+        }
+        ClearHighlightedTiles();
+
+        if (!activePlayer.CanMove(pointer)) {
+            return;
+        }
+
+        map.SetTileFlags(pointer, TileFlags.None);
+        map.SetColor(pointer, Color.blue);
+
+        highlightedTiles.Add(pointer);
+    }
+
+    private void ClearHighlightedTiles() {
+        foreach (var pos in highlightedTiles) {
+            map.SetColor(pos, Color.white);
+        }
+        highlightedTiles.Clear();
+    }
+
+
+    private void Awake() {
+        if (Instance == null) {
+            Instance = this;
+        }
+    }
+    void Start() {
+        
+    }
+
+    void Update() {
+        MouseLook();
+        if (Input.GetButtonDown("Fire1") && highlightedTiles.Count > 0) {
+            activePlayer.SetGridPos(highlightedTiles[0]);
+            ClearHighlightedTiles();
+        }
+    }
+
+    public int RegisterPlayer(PlayerController p) {
+        activePlayer = p;
+        playerControllers.Add(p);
+        int id = playerControllers.FindIndex(o => o == p);
+        Debug.Log("Registered Player #" + id);
+        return id;
+    }
+
+    public Vector3Int GridPos(Vector3 worldPos) {
+        return map.WorldToCell(worldPos);
+    }
+    public Vector3 WorldPos(Vector3Int gridPos) {
+        return map.CellToWorld(gridPos);
+    }
+}
--- ExplodingChicks/Assets/Player/PlayerController.cs
+++ ExplodingChicks/Assets/Player/PlayerController.cs
@@ -4,52 +4,26 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class PlayerController : MonoBehaviour {
-
-    [SerializeField]
-    private Tilemap map;
     
-    private List<Vector3Int> coloredTiles = new List<Vector3Int>();
+    private int id = -1;
 
-    // Start is called before the first frame update
+
     void Start() {
+        id = GameManager.Instance.RegisterPlayer(this);
         AlignToGrid();
     }
 
-    // Update is called once per frame
-    void Update() {
-        MouseLook();
-        if (Input.GetButtonDown("Fire1") && coloredTiles.Count > 0) {
-            transform.position = map.CellToWorld(coloredTiles[0]);
-            ClearColoredTiles();
-        }
-    }
 
-    void MouseLook() {
-        var pointerWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector3Int pointer = map.WorldToCell(pointerWorld);
-        pointer.z = 0;
-
-        if (coloredTiles.Contains(pointer)) {
-            return;
-        }
-        ClearColoredTiles();
-
-        var dist = Vector3Int.Distance(pointer, GridPos());
-        if (dist == 0 || dist > 1.5) {
-            return;
-        }
-
-        map.SetTileFlags(pointer, TileFlags.None);
-        map.SetColor(pointer, Color.blue);
-
-        coloredTiles.Add(pointer);
+    public bool CanMove(Vector3Int target) {
+        var dist = Vector3Int.Distance(target, GridPos());
+        return dist > 0 && dist < 1.5;
     }
 
     public Vector3Int GridPos() {
-        return map.WorldToCell(transform.position);
+        return GameManager.Instance.GridPos(transform.position);
     }
-    private void SetGridPos(Vector3Int pos) {
-        transform.position = map.CellToWorld(pos);
+    public void SetGridPos(Vector3Int pos) {
+        transform.position = GameManager.Instance.WorldPos(pos);
     }
 
     private void AlignToGrid() {
@@ -57,10 +31,4 @@ public class PlayerController : MonoBehaviour {
         SetGridPos(pos);
     }
 
-    private void ClearColoredTiles() {
-        foreach (var pos in coloredTiles) {
-            map.SetColor(pos, Color.white);
-        }
-        coloredTiles.Clear();
-    }
 }
