b689fb0f3d988d013eb6eecf959d8991a78379fd
Jakob Ruckel <jakob@ruckel.de>
2022-03-26 20:17:25 +0100

    🐎

--- ExplodingChicks/Assets/Player/Horse.cs
+++ ExplodingChicks/Assets/Player/Horse.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class Rook : PlayerController {
+public class Horse : PlayerController {
     public override string GetName() {
-        return "Rook";
+        return "Horse";
     }
     public override bool CanMove(Vector3Int target) {
 
         var dist = Vector3Int.Distance(target, GridPos());
         
-        int dist_x = target.x - GridPos().x;
-        int dist_y = target.y - GridPos().y;
+        int dist_x = Mathf.Abs(target.x - GridPos().x);
+        int dist_y = Mathf.Abs(target.y - GridPos().y);
 
-        return dist > 0 && (dist_x == 0 || dist_y == 0);
+        return (dist_x == 1 && dist_y == 2) || (dist_x == 2 && dist_y == 1);
     }
 }
